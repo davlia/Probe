456a238cffc3266d7f201719f21dbbb27f7b9d89
Danedaworld <danedaworld@gmail.com>
2014-07-21 23:57:15 -0400

    added screen capture

--- main.cpp
+++ main.cpp
@@ -1,14 +1,60 @@
-#include <opencv2/core/core.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/highgui/highgui.hpp>
-
+#include <Windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
 using namespace cv;
 
-int main()
-{
-    Mat image;// new blank image
-    image = cv::imread("C:/Users/David/Desktop/Programming/OpenCVTest/pikachu.png", 0);// read the file
-    namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// create a window for display.
-    imshow( "Display window", image );// show our image inside it.
-    waitKey(0);// wait for a keystroke in the window
-    return 0;
+
+Mat hwnd2mat(HWND hwnd){
+
+    HDC hwindowDC,hwindowCompatibleDC;
+
+    int height,width,srcheight,srcwidth;
+    HBITMAP hbwindow;
+    Mat src;
+    BITMAPINFOHEADER  bi;
+
+    hwindowDC=GetDC(hwnd);
+    hwindowCompatibleDC=CreateCompatibleDC(hwindowDC);
+    SetStretchBltMode(hwindowCompatibleDC,COLORONCOLOR);
+
+    RECT windowsize;    // get the height and width of the screen
+    GetClientRect(hwnd, &windowsize);
+
+    srcheight = windowsize.bottom;
+    srcwidth = windowsize.right;
+    height = windowsize.bottom/2;  //change this to whatever size you want to resize to
+    width = windowsize.right/2;
+
+    src.create(height,width,CV_8UC4);
+
+    // create a bitmap
+    hbwindow = CreateCompatibleBitmap( hwindowDC, width, height);
+    bi.biSize = sizeof(BITMAPINFOHEADER);    //http://msdn.microsoft.com/en-us/library/windows/window/dd183402%28v=vs.85%29.aspx
+    bi.biWidth = width;
+    bi.biHeight = -height;  //this is the line that makes it draw upside down or not
+    bi.biPlanes = 1;
+    bi.biBitCount = 32;
+    bi.biCompression = BI_RGB;
+    bi.biSizeImage = 0;
+    bi.biXPelsPerMeter = 0;
+    bi.biYPelsPerMeter = 0;
+    bi.biClrUsed = 0;
+    bi.biClrImportant = 0;
+
+    // use the previously created device context with the bitmap
+    SelectObject(hwindowCompatibleDC, hbwindow);
+    // copy from the window device context to the bitmap device context
+    StretchBlt( hwindowCompatibleDC, 0,0, width, height, hwindowDC, 0, 0,srcwidth,srcheight, SRCCOPY); //change SRCCOPY to NOTSRCCOPY for wacky colors !
+    GetDIBits(hwindowCompatibleDC,hbwindow,0,height,src.data,(BITMAPINFO *)&bi,DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+	DeleteObject (hbwindow); DeleteDC(hwindowCompatibleDC); ReleaseDC(hwnd, hwindowDC);
+    return src;
+
+}
+
+int main() {
+	hwnd2mat(0);
 }
